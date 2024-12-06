eaa689153414518aecf35ed2dcc6527f6551a7fd
Vamsi Krishna Kodimela <55780006+vamsi-krishna-kodimela@users.noreply.github.com>
2024-12-06 18:46:16 +0000

    Minor tweaks in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -22,10 +22,12 @@ const Navbar = () => {
     },
   ];
   return (
-    <nav className="p-8 px-4 shadow h-100 hidden md:block">
+    <nav className="p-8 px-6 shadow h-100 hidden md:block">
       <div className="flex gap-2 items-center">
         <img className="w-8" src={Logo} alt="Simplified.ninja" />
-        <h1 className="font-medium text-xl">Simplified.ninja</h1>
+        <h1 className="font-semibold text-2xl text-slate-600">
+          Simplified<span className="text-indigo-500">.ninja</span>
+        </h1>
       </div>
       <ul className="mt-8 flex flex-col gap-2">
         {links.map((link) => (
