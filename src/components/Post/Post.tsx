b15f79d651b6755f11e349a4037972792644b69b
Vamsi Krishna Kodimela <55780006+vamsi-krishna-kodimela@users.noreply.github.com>
2024-12-08 03:41:59 +0000

    Integrated Feed

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PostType from "../../types/Post";
 import { Eye, Heart, MessageCircle, MoreVertical, Share } from "react-feather";
-import moment from "moment";
 import formatNumber from "../../utils/format-number.util";
 
 type IPost = {
@@ -13,33 +12,28 @@ const Post = ({ post }: IPost) => {
     <div className="shadow rounded-lg p-4">
       <div className="flex gap-2 items-center border-b pb-2">
         <div className="rounded-full overflow-hidden w-8 h-8">
-          <img src={post.user.avatar_url} alt={post.user.username} />
+          <img src="https://picsum.photos/id/13/100/100" alt={post.title} />
         </div>
         <div className="flex-1">
-          <h6 className="text-sm font-semibold text-slate-600">
-            {post.user.username}
-          </h6>
-          <p className="text-xs text-slate-400">
-            {moment(post.published_on).format("DD MMM YYYY, hh:mm A")}
-          </p>
+          <h6 className="text-sm font-semibold text-slate-600">{post.title}</h6>
+          <p className="text-xs text-slate-400">{post.userId}</p>
         </div>
         <div>
           <MoreVertical className="w-4 text-slate-600" />
         </div>
       </div>
-      <div className="py-4 text-slate-600">{post.content}</div>
+      <div className="py-4 text-slate-600">{post.body}</div>
       <div className="pt-2 border-t flex items-center gap-4 justify-start text-slate-400">
         <button className="text-xs flex items-center gap-1">
-          <Eye className="w-4 h-4" />{" "}
-          <div>{formatNumber(post.views_count)}</div>
+          <Eye className="w-4 h-4" /> <div>{formatNumber(post.userId)}</div>
         </button>
         <button className="text-xs flex items-center gap-1">
           <Heart className="w-4 h-4" />
-          {formatNumber(post.likes_count)}
+          {formatNumber(post.userId)}
         </button>
         <button className="text-xs flex items-center gap-1">
           <MessageCircle className="w-4 h-4" />
-          {formatNumber(post.comments_count)}
+          {formatNumber(post.userId)}
         </button>
         <div className="flex-1"></div>
         <button>
--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,30 +1,8 @@
 type PostType = {
-  id: number; // Unique identifier for each post
-  user: {
-    id: number; // Unique identifier for the user
-    username: string; // User's username
-    avatar_url: string; // URL to the user's avatar
-  };
-  published_on: string; // ISO 8601 timestamp
-  content: string; // Content of the post
-  images: string[]; // Array of image URLs
-  post_type: string; // "quote" | "photo" | "update"; // Type of the post
-  likes_count: number; // Total number of likes
-  comments_count: number; // Total number of comments
-  views_count: number; // Total number of views
-  likes: {
-    id: number; // Unique identifier for the user who liked
-    username: string; // Username of the user who liked
-  }[]; // Array of users who liked the post
-  comments: {
-    user_id: number; // Unique identifier for the user who commented
-    username: string; // Username of the user who commented
-    comment: string; // Text of the comment
-  }[]; // Array of comments
-  views: {
-    id: number; // Unique identifier for the user who viewed
-    username: string; // Username of the user who viewed
-  }[]; // Array of users who viewed the post
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 };
 
 export default PostType;
